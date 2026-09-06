315c6ece748d4e06dc7c32506566ccb057723e53
Anup <anupgtm07@gmail.com>
2026-09-06 23:53:08 +0545

    name changed

--- backend/api/expense_api.py
+++ backend/api/expense_api.py
@@ -18,7 +18,7 @@ expense_api=APIRouter(prefix="/expense",tags=["expense endpoints"])
 async def create_expense_endpoint(data:ExpenseCreate,service:Annotated[ExpenseService,Depends(get_expense_service)]):
     await service.create_expense_service(data)
     return "Expense created successfully"
-@expense_api.get("/{expense_id}",response_model=SingleExpense) #expense_id is the path parameter and response model is used to validate the row
+@expense_api.get("/{expense_id}",response_model=SingleExpense) #expense_id is the path parameter and response model is used to validate row
 async def get_single_expense_endpoint(expense_id:UUID,service:Annotated[ExpenseService,Depends(get_expense_service)]):
     expenses=await service.get_single_expense(expense_id)
     return expenses
