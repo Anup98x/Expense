a3adf260a2ead170c6b1dc2da0543db5a1e5dc11
Anup <anupgtm07@gmail.com>
2026-09-12 23:13:11 +0545

    comments add

--- backend/dependencies/get_user.py
+++ backend/dependencies/get_user.py
@@ -14,7 +14,7 @@ from model.user import User
 #it is used to protect the endpoints and it decodes the token and gives user row for any endpoints that is using this dependencies
 async def get_user(
     request: Request,
-    # token: Annotated[str, Depends(OAuth2PasswordBearer(tokenUrl="/login"))], Annotated means the type of data
+    # token: Annotated[str, Depends(OAuth2PasswordBearer(tokenUrl="/login"))], Annotated means the type of data:
 
     db: Annotated[AsyncSession, Depends(get_db)],
 ):
