277053f8992f7345c0f623529c47a1523f61ff83
Anup <anupgtm07@gmail.com>
2026-09-12 23:14:56 +0545

    comments add

--- backend/repo/expense_repo.py
+++ backend/repo/expense_repo.py
@@ -51,4 +51,4 @@ class ExpenseRepo:
     async def delete_expense(self,expense:Expense):
 
         await self.session.delete(expense)
-        await self.session.commit()
+        await self.session.commit() #saves to database
