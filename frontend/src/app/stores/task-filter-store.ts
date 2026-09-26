cc85875c3f5076a6d49e7fad74eb527f521932ef
Anup <anupgtm07@gmail.com>
2026-09-26 22:26:37 +0545

    task filter store initalized

--- frontend/src/app/stores/task-filter-store.ts
+++ frontend/src/app/stores/task-filter-store.ts
@@ -8,7 +8,7 @@ interface TaskFilterState {
   setStatus: (status: TaskStatus | "all") => void;
   reset: () => void;
 }
-// using this function
+// using this functions
 export const useTaskFilterStore = create<TaskFilterState>((set) => ({
   search: "",
   status: "all",
